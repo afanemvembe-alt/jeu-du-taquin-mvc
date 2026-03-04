f0550e37ede188365d8dfe32f976e241abfb5260
Adji Marema LO <adji-marema.lo@etu.unicaen.fr>
2026-03-04 17:00:04 +0100

    Ajout du diagramme de classes

--- livraison/src/modele/Demo.java
+++ livraison/src/modele/Demo.java
@@ -6,3 +6,13 @@ public class Demo{
         System.out.println(tableau);
     }
 }
+
+
+
+
+
+/**Le sujet demande une réalisation "intégralement MVC" avec un "modèle totalement indépendant de l'interface graphique". 
+ * Le fichier Taquin.java est ton Modèle. Il contient uniquement des chiffres et de la logique (le "cerveau").
+ * Le JPanel (ou la JFrame) appartient à la Vue. Tu devras créer une autre classe (par exemple TaquinGraphique.java) qui, 
+ * elle, contiendra les JPanel et les boutons.
+*/
--- livraison/src/modele/Taquin.java
+++ livraison/src/modele/Taquin.java
@@ -4,7 +4,18 @@ public class Taquin{
     private int colonne;
     private int [][] tableau;
 
+//  on stocke la position de la case vide (le 0) pour savoir quoi déplacer
+    private int videL;
+    private int videC;
+
+
+
+
+
+
     public Taquin(int line, int row){
+         this.ligne = line;
+         this.colonne = row;
         int [][] table= new int[line][row];
         int valeur= 1;
         for(int i=0; i<table.length; i++){
@@ -16,7 +27,42 @@ public class Taquin{
         table[line-1][row-1]= 0;
         this.tableau= table;
 
+
+        // Initiliser la position du vide (en bas à droite au départ)
+        this.videL = line - 1;
+        this.videC = row - 1;
+
     }
+// deplacement de la piece situé en (l ,c) vers la case vide si elle est adjacente  et return true si le mouvement est validé et effectuer
+/**
+ * verifier si la case est bien dans les limites du tableau
+ * calcul de la distance avec la case vide 
+ * deplacement est possible si la case est à coté du vide (pas en diagonale)
+ * echange des valeurs 
+ * mettre à jour la position 
+ */
+// il y'a aussi le melange du puzzle par des glissements 
+
+
+
+
+  // méthode pour les Getters par ce que la vue aura besoin de regarder dans le modele pour savoir quels chiffres afficher
+public int getValeur(int l, int c) { return tableau[l][c]; }
+public int getLigne() { return ligne; }
+public int getColonne() { return colonne; }
+
+
+// est possible est pour verifier si l'echange ou le mouvement est posssible 
+public boolean estPossible(int ligne, int colonne);
+// pour echanger 2 valeurs (necessairement la valeur 0)
+public void echange(int valeur1, int valeur2);
+
+
+
+
+//monter ,descendre, gauche, droite
+
+
 
     public String toString(){
         String res= "";
