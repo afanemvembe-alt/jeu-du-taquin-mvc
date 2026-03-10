6d458294e5c91ab50f49238d708342da65d608ba
Adji Marema LO <adji-marema.lo@etu.unicaen.fr>
2026-03-10 12:44:46 +0100

    Creation d'un nouveau Demo2

---
+++ livraison/src/taquin/modele/Demo2.java
@@ -0,0 +1,26 @@
+package taquin.modele;
+
+
+public class Demo2 {
+    public static void main(String[] args) {
+        Taquin tableau = new Taquin(4, 5);
+        tableau.melanger(100);
+        System.out.println (tableau);
+        System.out.println(tableau.fini());
+
+        Taquin tableau2 = new Taquin(4, 5);
+        //tableau.melanger(100);
+        System.out.println (tableau2);
+        System.out.println(tableau2.fini());
+
+        Taquin tableau3 = new Taquin(7, 5);
+        tableau3.melanger(100);
+        System.out.println (tableau3);
+        System.out.println(tableau3.fini());
+
+        Taquin tableau4 = new Taquin(4, 2);
+        //tableau.melanger(100);
+        System.out.println (tableau4);
+        System.out.println(tableau4.fini());
+    }
+}
