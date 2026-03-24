d1ce9735b9f0f3d2187b823dfe3b37d667420a57
Adji Marema LO <adji-marema.lo@etu.unicaen.fr>
2026-03-24 10:56:23 +0100

    Ajout JavaDoc dans le fichier DemoConsole

--- livraison/src/taquin/controle/DemoConsole.java
+++ livraison/src/taquin/controle/DemoConsole.java
@@ -2,7 +2,19 @@ package taquin.controle;
 
 import taquin.modele.*;
 
+
+/*
+ * Classe de démonstration permettant de lancer le jeu du taquin
+ * en mode console. 
+ * Cette classe contient la méthode principale (main) qui initialise
+ * le jeu avec un plateau donné et démarre l'exécution.*/
+
 public class DemoConsole{
+ /**
+     * Méthode principale qui lance le programme.
+     * Elle cree un plateau de taquin avec des dimensions definies,
+     * initialise le contrôleur console, puis demarre le jeu.
+     */
 	public static void main(String[] args){
         Taquin tableau= new Taquin(4, 5);
         TaquinControleConsole jeu= new TaquinControleConsole(tableau);
